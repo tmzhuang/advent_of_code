3bae749b295170f63e7cbc438e12dcc8a01b4d64
Tianming Zhuang <tianming.zhuang@gmail.com>
2019-12-11 16:24:12 -0500

    Completes day 10 part 2.

--- 2019/day10/day10/__main__.py
+++ 2019/day10/day10/__main__.py
@@ -1,10 +1,23 @@
+import sys
+import logging
 from day10.utils import *
 def main():
+    logging.basicConfig(
+        # level=logging.DEBUG,
+        level=logging.INFO,
+        format='%(message)s',
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
+    logger = logging.getLogger()
+
     a = get_array('input.txt')
+    # a = get_array('input.txt')
     asteroids = get_asteroids(a)
-    count, asteroid, vectors, reduced = best_asteroid(asteroids)
-    print(count)
-    order_asteroids(asteroid, vectors, reduced)
+    count, asteroid, vectors, reduced, unique = best_asteroid(asteroids)
+    # print(count)
+    print(f'Base is situated on {asteroid}.')
+    sorted_asteroids = order_asteroids(asteroid, vectors, reduced, unique)
+    print(f'200th asteroid: {sorted_asteroids[199]}')
 
 
 if __name__ == '__main__':
